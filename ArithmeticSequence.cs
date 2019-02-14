af3b6eb0d0b058fc241245e976bd4d795d363c06
AnnZh <ann.zhurok@gmail.com>
2019-02-14 12:17:07 +0300

    Done

--- ArithmeticSequence.cs
+++ ArithmeticSequence.cs
@@ -7,7 +7,22 @@ namespace PadawansTask2
         public static int Calculate(int number, int add, int count)
         {
             // put your code here
-            throw new NotImplementedException();
+            //throw new NotImplementedException();
+
+            if (number > int.MaxValue || add > int.MaxValue || count > int.MaxValue)
+                throw new ArgumentException();
+            if (number <= 0 || add <= 0 || count <= 0)
+                throw new ArgumentException();
+
+            int n = number;
+            
+
+            for (int i = 0; i < count - 1; i++)
+            {
+                number = number + (n + (add * (i + 1)));
+            }
+
+            return number;
         }
     }
 }
